abbdb24fe7585e99fd6d8d9f12cb151beca6abc3
Samer <samer@woven.org>
2012-07-28 00:31:12 -0400

    Nodejs / Derby fun!

--- activity-derby/lib/app/index.js
+++ activity-derby/lib/app/index.js
@@ -8,7 +8,10 @@ var derby = require('derby')
 derby.use(require('../../ui'))
 
 
-// ROUTES //
+
+process.nextTick(function () {
+    console.log('nextTick callback');
+});
 
 // Derby routes can be rendered on the client and the server
 get('/rooms/:roomName?', function(page, model, params) {
@@ -58,15 +61,23 @@ get('/activities', function(page, model, params) {
     };
     */
 
+    // @todo: https://github.com/molnarg/js-schema/blob/master/README.md
+    // @todo: http://stackoverflow.com/questions/5311334/what-is-the-purpose-of-nodejs-module-exports-and-how-do-you-use-it
+    // @todo: http://visionmedia.github.com/masteringnode/book.html
+    // @todo: http://stackoverflow.com/questions/5722638/node-js-rss-module
+    // @todo: http://howtonode.org/understanding-process-next-tick
+
+    // ROUTES //
+    var test = ([
+        {'content': "test1", "comments": [{content:'test1',author:"Samer"},{content:'test3',author:'David'}] },
+        {'content': "test5", "comments": [{content:'test1',author:"Samer"},{content:'test3',author:'David'}] }
+    ]);
+
+    model.set("_newActivity","HERE");
+
+    model.setNull('activities.list',test);
+
     model.subscribe('activities', function(err, model) {
-
-        var test = ([
-            {'content': "test1", "comments": [{content:'test1',author:"Samer"},{content:'test3',author:'David'}] },
-            {'content': "test5", "comments": [{content:'test1',author:"Samer"},{content:'test3',author:'David'}] }
-        ]);
-
-        model.setNull('list',test);
-
         console.log('app.get.subscribe');
         page.render();
     });
@@ -81,4 +92,11 @@ ready(function(model) {
         console.log('ready.model.on.push');
     });
 
+    app.postActivity = function (){
+        console.log("here yeah here");
+        comment = model.get("_newActivity");
+        $test = model.push("activities.list",
+            {'content': comment, "comments": [{content:'MyComment',author:"I'm His User"},{content:'Cool',author:'David'}]}
+        );
+    }
 })
